0b5c3c20cbc84345cbfec9103d550ee22a47695a
Rokas Puzonas <rokas.puz@gmail.com>
2022-03-13 20:45:12 +0200

    feat: adjustable camera

--- main.cc
+++ main.cc
@@ -37,6 +37,7 @@ int main() {
 	const int max_depth = 32;
 
 	// World
+	auto R = cos(pi/4);
 	hittable_list world;
 
 	auto material_ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
@@ -47,11 +48,11 @@ int main() {
 	world.add(make_shared<sphere>(point3( 0, -100.5, -1), 100, material_ground));
 	world.add(make_shared<sphere>(point3( 0,      0, -1), 0.5, material_center));
 	world.add(make_shared<sphere>(point3(-1,      0, -1), 0.5, material_left));
-	world.add(make_shared<sphere>(point3(-1,      0, -1), -0.4, material_left));
+	world.add(make_shared<sphere>(point3(-1,      0, -1), -0.45, material_left));
 	world.add(make_shared<sphere>(point3( 1,      0, -1), 0.5, material_right));
 
 	// Camera
-	camera cam;
+	camera cam(point3(-2, 2, 1), point3(0, 0, -1), vec3(0, 1, 0), 70, aspect_ratio);
 
 	// Render
 	std::cout<<"P3\n" <<image_width<<" "<<image_height<<"\n255\n";
